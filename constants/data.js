c4008263ee23bfa088482225b26e77eaef331725
Aman Singh <146389653+aman40399@users.noreply.github.com>
2024-08-25 16:23:23 +0530

    Add files via upload

---
+++ constants/data.js
@@ -0,0 +1,109 @@
+const userData = {
+  githubUsername: "aman40399",
+  name: "Aman Singh..",
+  designation: "AIML-ENGINNER | RESEARCHER | DEVELOPER",
+  avatarUrl: "/avatar.png",
+  email: "nevermind40399@gmail.com",
+  phone: "+91 9773978753",
+  address: "GREATER NOIDA, India.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a 🪐 Innovative  AIML Engineer | MLOps Specialist , creator of PROJECT-CORTANA 🤖 ",
+    description: [
+      `Enthusiastic and skilled B.Tech student in Artificial Intelligence and Machine Learning, with a strong proficiency in Python and a proven track record of developing impactful AI-driven projects. Successfully engineered a custom chatroom application and a robust file management system, demonstrating practical experience in web technologies and secure file handling. Adept at utilizing modern tools like Neovim and Linux-based utilities for efficient development. Passionate about advancing the field of AIML through hands-on project experience, continuous learning, and a dedication to leveraging emerging technologies to solve real-world challenges. Eager to contribute to innovative AIML solutions and drive progress in the field.`,
+    ],
+    currentProject: "BLUE-BIRD",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "SELF EMPLOYED",
+      company: "RESEARCH AND DEVELOPMENT | PROJECT CREATION ",
+      year: "2024",
+      companyLink: "",
+      desc: "DEVELOPED PROJECT CORTANA , WRITING RESEARCH PAPERS , READING PAPERS .",
+    },
+    {
+      title: "Freelance",
+      company: "FIVERR",
+      year: "2023-24",
+      companyLink: "https://fiverr.com",
+      desc: "Worked with 3 indiviual clients to make there own brand devleoped site && used automation , done work with founders and several small creators .",
+    },
+    {
+      title: "Internship",
+      company: "JIT TRAININGS ( Uinited States , remote )",
+      year: "2023",
+      companyLink: "https://jittrainings.com",
+      desc: "Worked as a web dev intern.",
+    },
+    {
+      title: "Graduation (PRESENT)",
+      company: "SHARDA UNIVERSITY.",
+      year: "2027",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering IN sp with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "AVB Public School, NEW DELHI.",
+      year: "2022",
+      companyLink: "https://avbps.in",
+      desc: "Subjects being PCMBC - I barely survived with 82.0% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/aman.40399",
+    twitter: "https://twitter.com/aman40399",
+    linkedin: "https://linkedin.com/in/aman40399",
+    github: "https://github.com/aman40399",
+    facebook: "https://facebook.com/aman40399",
+    peerlist: "https://peerlist.io/aman40399",
+  },
+};
+
+export default userData;
